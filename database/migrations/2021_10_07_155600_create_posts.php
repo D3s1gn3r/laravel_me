c65676cd5a7408246d37cc230893eabd0f6a8b1d
Evgeniy Galyuk <evgeniy.galyuk@gmail.com>
2021-10-07 19:16:02 +0300

    create post and category posts migrations

---
+++ database/migrations/2021_10_07_155600_create_posts.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('category_id')->unsigned();
+            $table->string('title');
+            $table->string('username');
+            $table->string('email');
+            $table->text('post_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
